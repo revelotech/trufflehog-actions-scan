48819b8e6d814189211fd72c8ff3819aa66bae2b
Lucas Vieira <lucas.vieira@revelo.com.br>
2020-08-05 17:45:04 -0300

    Change max_depth strategy

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 set -e # Abort script at first error
 
 current_branch=$(git rev-parse --abbrev-ref HEAD)
-max_depth=$(git rev-list --count HEAD ^master)
+max_depth=$(git rev-list --count 'HEAD..master')
 args="--regex --branch ${current_branch} --max_depth=${max_depth}" # Default trufflehog options
 
 if [ -n "${INPUT_SCANARGUMENTS}" ]; then
