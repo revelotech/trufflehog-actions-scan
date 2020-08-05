847bf3b686627be57b0654bc6e982db1723cdbc8
Lucas Vieira <lucas.vieira@revelo.com.br>
2020-08-05 17:06:06 -0300

    Default to current branch

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,18 +1,13 @@
 #!/usr/bin/env sh
 set -e # Abort script at first error
 
-args="--regex --max_depth=50" # Default trufflehog options
+current_branch=$(git rev-parse --abbrev-ref HEAD)
+max_depth=$(git rev-list --count master..HEAD)
+args="--regex --branch ${current_branch} --max_depth=${max_depth}" # Default trufflehog options
 
 if [ -n "${INPUT_SCANARGUMENTS}" ]; then
   args="${INPUT_SCANARGUMENTS}" # Overwrite if new options string is provided
 fi
-
-# if [ -n "${INPUT_GITHUBTOKEN}" ]; then
-#   githubRepo="https://$INPUT_GITHUBTOKEN@github.com/$GITHUB_REPOSITORY" # Overwrite for private repository if token provided
-# else
-#   githubRepo="https://github.com/$GITHUB_REPOSITORY" # Default target repository
-# fi
-
 
 query="$args" # Build args query with repository url
 trufflehog $query .
