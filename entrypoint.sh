9d994f4d78d84461958dc44e5098a0e02bb5af60
Lucas Vieira <lucas.vieira@revelo.com.br>
2020-08-05 13:43:10 -0300

    Initial

---
+++ entrypoint.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+set -e # Abort script at first error
+
+args="--regex --entropy=False --max_depth=50" # Default trufflehog options
+
+if [ -n "${INPUT_SCANARGUMENTS}" ]; then
+  args="${INPUT_SCANARGUMENTS}" # Overwrite if new options string is provided
+fi
+
+if [ -n "${INPUT_GITHUBTOKEN}" ]; then
+  githubRepo="https://$INPUT_GITHUBTOKEN@github.com/$GITHUB_REPOSITORY" # Overwrite for private repository if token provided
+else
+  githubRepo="https://github.com/$GITHUB_REPOSITORY" # Default target repository
+fi
+
+query="$args $githubRepo" # Build args query with repository url
+trufflehog $query
