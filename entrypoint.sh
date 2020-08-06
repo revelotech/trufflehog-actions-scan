e82f2ad079a651fa6189e8ec3ac559a2235bc454
Lucas Vieira <lucas.vieira@revelo.com.br>
2020-08-06 10:41:44 -0300

    Add verbosity

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,8 @@
 set -e # Abort script at first error
 
 current_branch=$(git rev-parse --abbrev-ref HEAD)
-max_depth=$(git rev-list --count --no-merges master..)
+max_depth=$(git rev-list origin/master..HEAD --count)
+echo "Current branch: ${current_branch}. # commits: ${max_depth}."
 args="--regex --branch ${current_branch} --max_depth=${max_depth}" # Default trufflehog options
 
 if [ -n "${INPUT_SCANARGUMENTS}" ]; then
