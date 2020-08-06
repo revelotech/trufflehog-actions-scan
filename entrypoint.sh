773485bd6ade4c64c7755667bb38d5b137163ad1
Lucas Vieira <lucas.vieira@revelo.com.br>
2020-08-06 10:46:42 -0300

    json output

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ set -e # Abort script at first error
 current_branch=$(git rev-parse --abbrev-ref HEAD)
 max_depth=$(git rev-list origin/master..HEAD --count)
 echo "Current branch: ${current_branch}. # commits: ${max_depth}."
-args="--regex --branch ${current_branch} --max_depth=${max_depth}" # Default trufflehog options
+args="--regex --branch ${current_branch} --max_depth=${max_depth} --json" # Default trufflehog options
 
 if [ -n "${INPUT_SCANARGUMENTS}" ]; then
   args="${INPUT_SCANARGUMENTS}" # Overwrite if new options string is provided
