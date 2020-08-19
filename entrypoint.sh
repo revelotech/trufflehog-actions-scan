ec2550421ae68aa9606c78dabcd96c8267cd82ac
Lucas Vieira <lucas.vieira@revelo.com.br>
2020-08-19 10:51:59 -0300

    Do not fetch master

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 set -e # Abort script at first error
 
 # Ensure master ref is present
-git fetch -u origin master:master
+# git fetch -u origin master:master
 
 current_branch=$(git rev-parse --abbrev-ref HEAD)
 max_depth=$(git rev-list origin/master..HEAD --count)
@@ -17,10 +17,6 @@ fi
 args="--regex --branch ${current_branch} --json -x /.ignorelist" # Default trufflehog options
 if [[ $max_depth -gt 0 ]]; then
   args="$args --max_depth=${max_depth}"
-fi
-
-if [ -n "${INPUT_SCANARGUMENTS}" ]; then
-  args="${INPUT_SCANARGUMENTS}" # Overwrite if new options string is provided
 fi
 
 query="$args" # Build args query with repository url
