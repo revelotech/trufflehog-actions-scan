3ac98d8c024fd40cdc00b34793e067b71dff6791
Gabriel Lima <gabriel.lima@revelo.com.br>
2020-10-09 17:01:07 -0300

    Add check to rename branch if current_branch is a pr branch (HEAD)

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,6 +11,12 @@ if [[ "$current_branch" != 'master' && $max_depth -eq 0 ]]; then
   exit 0
 fi
 
+if [[ "$current_branch" == "HEAD" ]]; then
+  current_branch="check-secrets-from-pr"
+  echo "checking out to $current_branch"
+  git checkout -b $current_branch
+fi
+
 args="--regex --rules /regexes.json --branch ${current_branch} --json -x /.ignorelist" # Default trufflehog options
 if [[ $max_depth -gt 0 ]]; then
   args="$args --max_depth=${max_depth}"
