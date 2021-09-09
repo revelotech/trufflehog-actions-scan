4c0437753992ed4ef908f8788bd0d32a2b40ef14
Lucas Vieira <lucas.vieira@revelo.com.br>
2021-09-09 16:01:52 -0300

    Simplify conditionals

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,20 @@
 #!/usr/bin/env sh
 set -e # Abort script at first error
 
-if [[ -z $INPUT_DEFAULT_BRANCH ]]; then
+if [ -z $INPUT_DEFAULT_BRANCH ]; then
   INPUT_DEFAULT_BRANCH='master'
 fi
 
-if [[ -z $IGNORE_LIST_PATH ]]; then
+if [ -z $IGNORE_LIST_PATH ]; then
   IGNORE_LIST_PATH='/.ignorelist'
 fi
 
-if [[ -z $REGEXES_PATH ]]; then
+if [ -z $REGEXES_PATH ]; then
   REGEXES_PATH='/regexes.json'
+fi
+
+if [ -z $FOOBAR ]; then
+  echo 'foobar'
 fi
 
 current_branch=$(git rev-parse --abbrev-ref HEAD)
@@ -19,25 +23,25 @@ max_depth=$(git rev-list origin/${INPUT_DEFAULT_BRANCH}..HEAD --count)
 echo current branch is $current_branch
 echo max depth is $max_depth
 
-if [[ "$current_branch" != "$INPUT_DEFAULT_BRANCH" && $max_depth -eq 0 ]]; then
+if [ "$current_branch" != "$INPUT_DEFAULT_BRANCH" ] && [ $max_depth -eq 0 ]; then
   exit 0
 fi
 
-if [[ "$current_branch" == "HEAD" ]]; then
+if [ $current_branch = "HEAD" ]; then
   current_branch="check-secrets-from-pr"
   echo "checking out to $current_branch"
   git checkout -b $current_branch
 fi
 
 args="--regex --rules $REGEXES_PATH --branch ${current_branch} --json -x ${IGNORE_LIST_PATH}" # Default trufflehog options
-if [[ $max_depth -gt 0 ]]; then
+if [ $max_depth -gt 0 ]; then
   args="$args --max_depth=${max_depth}"
 fi
 
 query="$args" # Build args query with repository url
 echo running \"$query\"
 res=$(trufflehog $query . | jq -s 'del(.[].diff) |del(.[].printDiff) |del(.[].stringsFound)')
-if [[ "$res" != "[]" ]]; then
+if [ "$res" != "[]" ]; then
   echo $res | jq
   exit 1
 fi
