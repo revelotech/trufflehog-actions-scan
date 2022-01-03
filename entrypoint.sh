d85fb8b844f4c13e3ed2c1e54e9cba5c4a3bf13a
Lucas Vieira <lucas.vieira@revelo.com.br>
2022-01-03 20:18:42 -0300

    Keep newlines in original JSON

--- entrypoint.sh
+++ entrypoint.sh
@@ -62,7 +62,6 @@ fi
 echo ":: Running \"trufflehog $args .\""
 res=$(trufflehog $args . | jq -s "$JQ_QUERY" | jq -c .)
 if [ "$res" != "[]" ]; then
-  res=$( echo -E "$res" | tr -d '\\n')
   echo "::warning ::High entropy found"
   echo '::set-output name=high_entropy::'"$res"
 else
